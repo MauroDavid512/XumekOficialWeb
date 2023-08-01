9d06eedec383267a083f38636d4879b1ee0bac01
MauroDavid512 <mauroalosactormza@gmail.com>
2023-08-01 18:01:27 -0300

    deploy1

--- server/index.js
+++ server/index.js
@@ -1,44 +1,10 @@
-//                       _oo0oo_
-//                      o8888888o
-//                      88" . "88
-//                      (| -_- |)
-//                      0\  =  /0
-//                    ___/`---'\___
-//                  .' \\|     |// '.
-//                 / \\|||  :  |||// \
-//                / _||||| -:- |||||- \
-//               |   | \\\  -  /// |   |
-//               | \_|  ''\---/''  |_/ |
-//               \  .-\__  '-'  ___/-. /
-//             ___'. .'  /--.--\  `. .'___
-//          ."" '<  `.___\_<|>_/___.' >' "".
-//         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
-//         \  \ `_.   \_ __\ /__ _/   .-` /  /
-//     =====`-.____`.___ \_____/___.-`___.-'=====
-//                       `=---='
-//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const { createOrLoginUser } = require('./src/routes/utilsUsers.js')
-const { createCategory } = require('./src/routes/utilsCategories.js')
 
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
   server.listen(3001, () => {
-    createOrLoginUser({
-      name: "Carli",
-      lastName: "Segatore",
-      email:"carli@gmail.com",
-      id: "33",
-      admin: true
-    })
-    createCategory({
-      name: "policial"
-    })
-    createCategory({
-      name: "ddhh"
-    })
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+    console.log('%s listening at 3001');
   });
 });
